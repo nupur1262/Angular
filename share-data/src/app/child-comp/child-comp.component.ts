813cfeb5b9d703002811e9502b2b35eb7b55ab2f
Nupur <nupurpahuja09@gmail.com.com>
2023-04-22 05:13:59 +0530

    Parent to child component

---
+++ share-data/src/app/child-comp/child-comp.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child-comp',
+  templateUrl: './child-comp.component.html',
+  styleUrls: ['./child-comp.component.scss']
+})
+export class ChildCompComponent {
+  @Input()
+  parentVariable !: string;
+}
